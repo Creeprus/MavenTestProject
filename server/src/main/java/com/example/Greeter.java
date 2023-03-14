fbed6072cb9aaec23cce342715b62ecbb6fb4b00
Creeprus <92712690+Creeprus@users.noreply.github.com>
2023-03-14 09:02:48 +0300

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,19 +1,12 @@
 package com.example;
-
 /**
  * This is a class.
  */
-
-
-
-
 public class Greeter {
-
   /**
    * This is a constructor.
    */
   public Greeter() {
-
   }
 
 /**
@@ -21,21 +14,6 @@ public class Greeter {
  * @return greeting string
  **/
   public final String greet(final String someone) {
-    
-    
-    
-    
     return String.format("Hello, %s!", someone);
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
   }
 }
